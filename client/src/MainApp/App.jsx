fa78ffcec2b362f928e486c8959882caf8bf5eb5
Arvin Jay Tuazon <a.tuazon.122402@gmail.com>
2026-03-06 14:16:28 +0800

    -Added Site Alert jsx file and css

--- client/src/MainApp/App.jsx
+++ client/src/MainApp/App.jsx
@@ -1,5 +1,6 @@
 import HomePage from './Home_Page';
 import SMDashboard from './SMDashboard';
+import SADashboard from './SADashboard';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
         <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="/Storm_Master_List" element={<SMDashboard/>}/>
+            <Route path="/Site_Alert_Isolation" element={<SADashboard/>}/>
         </Routes>
     </Router>
   );
